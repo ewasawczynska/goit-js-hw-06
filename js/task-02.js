296bc4039177c24d2d209665cae9048e21e628d5
ewasawczynska <sawczynska.e@gmail.com>
2023-05-28 14:55:05 +0200

    task2/done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = element;
+  listItem.classList.add("item");
+  listOfIngredients.append(listItem);
+});
