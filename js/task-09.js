90c15684a083f8d5fa9cbc1ca5b793395930e44c
ewasawczynska <sawczynska.e@gmail.com>
2023-05-29 21:29:21 +0200

    task9/done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const setFocusBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const pageStyle = document.querySelector("style");
+const colorValue = document.querySelector(".color");
+const randomColor = getRandomHexColor();
+
+setFocusBtn.addEventListener("click", () => {
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
