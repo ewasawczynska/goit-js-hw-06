9abaa63e1cf0f5f90df6b89a61a167ad0ce974c7
ewasawczynska <sawczynska.e@gmail.com>
2023-05-29 20:35:07 +0200

    task7/done-but-not-working

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const inputZip = document.querySelector("#font-size-control");
+const outputText = document.querySelector("#text");
+
+inputZip.addEventListener("input", (event) => {
+  outputText.style.fontSize = event.currentTarget.value;
+});
