5710b73d506d363f8b5e753b20e79532249d79ae
ewasawczynska <sawczynska.e@gmail.com>
2023-05-29 18:40:12 +0200

    task4/in-progress2

--- js/task-04.js
+++ js/task-04.js
@@ -3,16 +3,16 @@ const value = document.querySelector("#value");
 
 const substract = (counterValue) => {
   counterValue -= 1;
-  return (value.textContent = counterValue);
+  value.textContent = counterValue;
 };
 
 const add = (counterValue) => {
   counterValue += 1;
-  return (value.textContent = counterValue);
+  value.textContent = counterValue;
 };
 
-const buttonMinus = document.querySelector("#counter.firstChild");
+const buttonMinus = document.querySelector("button:first-of-type");
 buttonMinus.addEventListener("click", substract);
 
-const buttonPlus = document.querySelector("#counter.lastChild");
+const buttonPlus = document.querySelector("button:last-of-type");
 buttonPlus.addEventListener("click", add);
