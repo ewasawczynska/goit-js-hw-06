fb7fe453bef3e6ffa8d41153b37b028af2b015c4
ewasawczynska <sawczynska.e@gmail.com>
2023-05-31 18:54:50 +0200

    task7/finished

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const inputZip = document.querySelector("#font-size-control");
 const outputText = document.querySelector("#text");
 
 inputZip.addEventListener("input", (event) => {
-  outputText.style.fontSize = event.currentTarget.value;
+  outputText.style.fontSize = event.currentTarget.value + "px";
 });
